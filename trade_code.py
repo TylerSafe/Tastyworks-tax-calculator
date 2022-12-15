a7841aee56b6eb59f3ec4c840ded176cd3c0ef8c
TylerSafe <98946533+TylerSafe@users.noreply.github.com>
2022-12-15 13:42:03 +0800

    Add files via upload
    - Added csv processing functionality
    - Created function to store relevant data in a dictionary with stock tickers as keys

--- trade_code.py
+++ trade_code.py
@@ -1,25 +1,42 @@
-from openpyxl import load_workbook
-import pathlib
+import csv
 
-PATH = pathlib.Path(__file__).parent.resolve()
+if __name__ == "__main__":
+    
+    #trade_date, root_symbol, trade_type, quantity, price, value, fees, total, remaining
+    def get_stock_data(row):
+        # remove commas from $ values
+        value = row["Value"].replace(",", "")
+        
+        # check if quantity is 0 for non trade entries
+        if float(row["Quantity"]) != 0:
+            price = float(value) / float(row["Quantity"])
+        else:
+            price = 0
 
-try:
-    wb = load_workbook(PATH + '\\History.csv')
+        # check if commissions is or has non numeric characters
+        if row["Commissions"].isnumeric() == False:
+            row["Commissions"] = 0
 
-    ws = wb['History']  
-    date = ws['A']
-    symbol = ws['M'] 
-    desc = ws['F']
-    value = ws['G']
-    qty = ws['H']
-    comm = ws['J']
-    fees = ws['K']
+        total_fees = float(row["Commissions"]) + float(row["Fees"])
+        net_cost = float(value) - total_fees
+        
+        # create list of relevant data
+        data = [row["Date"], row["Root Symbol"], row["Action"], row["Quantity"], price, value, total_fees,  net_cost, 0]
+        
+        return data
+        
+    # read in data from csv file
+    with open("History.csv", "r") as f:
+        reader = csv.DictReader(f)
+    
+        stocks = {}
 
+        # store data in dictionary of lists
+        for row in reader:
+            if row["Root Symbol"] in stocks.keys():
+                stocks[row["Root Symbol"]].append(get_stock_data(row))
+            else:
+                stocks[row["Root Symbol"]] = [get_stock_data(row)]
 
-    date = [ws['A'][x].value for x in range(len(ws['A']))]
-    del date[0]
-
-    print(date)
-
-except:
-    print("Error, no excel file found")
+    print(stocks["AAPL"])
+        
