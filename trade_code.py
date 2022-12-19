c927b620ad481dac4f030714db71f4ede0c5a149
TylerSafe <98946533+TylerSafe@users.noreply.github.com>
2022-12-19 12:48:50 +0800

    Add files via upload
    - Added beginnings of UI
    - Added drop down box with names of all traded stocks

--- trade_code.py
+++ trade_code.py
@@ -1,6 +1,65 @@
 import csv
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+import sys
+
+class Ui_Form(object):
+    def __init__(self) -> None:
+        self.comboBox = QtWidgets.QComboBox(Form)
+
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(614, 514)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(460, 90, 101, 31))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.PL = QtWidgets.QLabel(Form)
+        self.PL.setGeometry(QtCore.QRect(320, 60, 71, 21))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.PL.setFont(font)
+        self.PL.setObjectName("PL")
+        self.PL_2 = QtWidgets.QLabel(Form)
+        self.PL_2.setGeometry(QtCore.QRect(210, 60, 71, 21))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.PL_2.setFont(font)
+        self.PL_2.setObjectName("PL_2")
+        self.PL_3 = QtWidgets.QLabel(Form)
+        self.PL_3.setGeometry(QtCore.QRect(80, 60, 71, 21))
+        font = QtGui.QFont()
+        font.setPointSize(11)
+        self.PL_3.setFont(font)
+        self.PL_3.setObjectName("PL_3")
+        self.progressBar = QtWidgets.QProgressBar(Form)
+        self.progressBar.setGeometry(QtCore.QRect(470, 130, 118, 23))
+        self.progressBar.setProperty("value", 24)
+        self.progressBar.setObjectName("progressBar")
+        self.comboBox.setGeometry(QtCore.QRect(38, 190, 141, 22))
+        self.comboBox.setObjectName("comboBox")
+        
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Calculate"))
+        self.PL.setText(_translate("Form", "Total P/L"))
+        self.PL_2.setText(_translate("Form", "Sold"))
+        self.PL_3.setText(_translate("Form", "Bought"))
+
+    def add_menu_option(self, stock):
+        self.comboBox.addItem(stock)
 
 if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    
     
     #trade_date, root_symbol, trade_type, quantity, price, value, fees, total, remaining
     def get_stock_data(row):
@@ -38,5 +97,13 @@ if __name__ == "__main__":
             else:
                 stocks[row["Root Symbol"]] = [get_stock_data(row)]
 
-    print(stocks["AAPL"])
+                # check if symbol is blank
+                if row["Root Symbol"] == "":
+                    ui.add_menu_option("Non-Stock Related")
+                else:
+                    ui.add_menu_option(row["Root Symbol"])
+
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
         
